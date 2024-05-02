475517ab2aa1800a7f3c598385c00b4cc29da2e3
Rory Fletcher <roryfletcher2002@gmail.com>
2024-05-02 09:37:04 +0100

    10 & 11 and some feedback

--- models/news-model.js
+++ models/news-model.js
@@ -15,32 +15,34 @@ const fetchArticle = (article_id) => {
             return rows[0];
         });
 }
-const fetchAllArticles = (topic, sort_by='created_at', order='DESC') => {
-    let sqlQuery = `SELECT
-        articles.article_id,
-        articles.title,
-        articles.topic,
-        articles.author,
-        articles.created_at,
-        articles.votes,
-        articles.article_img_url,
-        COUNT(comments.article_id)::INT AS comment_count
+const fetchAllArticles = (topic) => {
+    const queryValues = []
+    let sqlQueryString = 
+        `SELECT articles.author,
+            articles.title, 
+            articles.article_id,
+            articles.topic,
+            articles.created_at,
+            articles.votes,
+            articles.article_img_url,
+        COUNT(comment_id) :: INT AS comment_count
         FROM articles
         LEFT JOIN comments
-        ON articles.article_id = comments.article_id `;
-
-    let queryVals = [];
+        ON comments.article_id = articles.article_id `
 
     if (topic) {
-        sqlQuery += `WHERE articles.topic = $1 `;
-        queryVals.push(topic);
+        queryValues.push(topic)
+        sqlQueryString += 
+        `WHERE topic=$1 `
     }
 
-    sqlQuery += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`;
+    sqlQueryString += 
+        `GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;`
 
-    return db.query(sqlQuery, queryVals)
+    return db.query(sqlQueryString, queryValues)
     .then(({ rows }) => {
-        return { articles: rows };
+        return {articles: rows};
     });
 };
 
